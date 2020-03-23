909f9b8b1070f04ef4c2116d6773a224a6fbd758
yuki takagi <yuki99takagi@gmail.com>
2020-03-23 10:44:10 +0900

    Tweet削除機能を追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
   def index
     @tweets = Tweet.all
   end
@@ -27,8 +28,15 @@ class TweetsController < ApplicationController
       render :edit
     end
   end
+  def destroy
+    @tweet.destroy
+    redirect_to tweets_path, notice: "Tweetを削除しました！"
+  end
   private
   def tweet_params
     params.require(:tweet).permit(:content)
+  end
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 end
