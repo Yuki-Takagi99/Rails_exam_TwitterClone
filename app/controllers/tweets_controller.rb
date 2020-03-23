dc60c74476f5bf9e17b542a3113f6b3585208b6d
yuki takagi <yuki99takagi@gmail.com>
2020-03-23 10:26:45 +0900

    Tweet編集画面を作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,11 +6,26 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new
   end
   def create
-    Tweet.create(tweet_params)
-    redirect_to new_tweet_path
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to tweets_path, notice: "Tweetを作成しました！"
+    else
+      render :new
+    end
   end
   def show
     @tweet = Tweet.find(params[:id])
+  end
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+  def update
+    @tweet = Tweet.find(params[:id])
+    if @tweet.update(tweet_params)
+      redirect_to tweets_path, notice: "Tweetを編集しました！"
+    else
+      render :edit
+    end
   end
   private
   def tweet_params
